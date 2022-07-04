cb8951bfee3099ac155232455e950ae967b1a098
rakeshrewale <rakeshrewale993@gmail.com>
2022-07-04 15:21:57 +0530

    Added: Added Method is applied in linklist

---
+++ LinkedListMain.java
@@ -0,0 +1,17 @@
+package com.Linklist;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+
+		LinkedList linkedlist = new LinkedList();
+
+		linkedlist.addFirst(70);
+		linkedlist.display();
+		linkedlist.addFirst(30);
+		linkedlist.display();
+		linkedlist.addFirst(56);
+		linkedlist.display();
+	}
+
+}
